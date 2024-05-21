c1a9495ef90998386a30103d4d6f65a50dbc801e
ayman <amsaoub@e3r8p13.1337.ma>
2024-05-21 18:19:10 +0100

    start page 1.1

--- ft_trans/src/components/Nav_Bar/Nav_Bar.tsx
+++ ft_trans/src/components/Nav_Bar/Nav_Bar.tsx
@@ -1,18 +1,18 @@
 import React from "react";
-import './Nav_Bar.css'
+import "./Nav_Bar.css";
 
 function Nav_Bar() {
   return (
     <>
       <div className="main_div">
         <div className="box_left">
-          <img id="logo" src='Home_page/logo.png' alt="logo" />
+          <img id="logo" src="Home_page/logo.png" alt="logo" />
         </div>
         <div className="box_right">
           <div className="menu">
             <img id="img_menu" src="Home_page/lines.svg" alt="logo" />
           </div>
-        <ul className="nav_list">
+          <ul className="nav_list">
             <li>Home</li>
             <li>Discover</li>
             <li>About</li>
--- ft_trans/src/components/Page_One/Page_One.tsx
+++ ft_trans/src/components/Page_One/Page_One.tsx
@@ -6,15 +6,16 @@ function Page_One() {
   return (
     <>
       <div className="main_p_1">
-        {/* <Nav_Bar   /> */}
+        <Nav_Bar   />
         <div className="dev_text">
           <div className="text_1">
             <p>Welcome to the </p> <p>World of Ping Pong</p>
           </div>
           <div className="text_2">Best Game of 2024</div>
-            <button id="btn_play">Play Now !</button>
+            <button id="btn_play_now">Play Now !</button>
         </div>
-        {/* <div className="ft_p1"><img src="Home_page/wave.png" alt="" /></div> */}
+        <div className="div_svg"></div>
+        {/* <div className="ft_p1"><img src="Home_page/wave.png" alt="wave" /></div> */}
       </div>
     </>
   );
