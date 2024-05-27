4c13f422424e275858aa4ae68495844736a3b9b2
ayman <amsaoub@e1r5p1.1337.ma>
2024-05-27 17:53:13 +0100

    finished pages without side_bar

--- ft_trans/src/App.tsx
+++ ft_trans/src/App.tsx
@@ -1,27 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Nav_Bar from './components/Nav_Bar/Nav_Bar'
-import Page_One from './components/Page_One/Page_One'
-import Page_Two from './components/Page_Two/Page_Two'
-import Squar_P2_Small from './components/Squar_P2_Small/Squar_P2_Small'
-
-
+import Nav_Bar from "./components/Nav_Bar/Nav_Bar";
+import Page_One from "./components/Page_One/Page_One";
+import Page_Two from "./components/Page_Two/Page_Two";
+import Page_Three from "./components/Page_Three/Page_Three";
+import "./App.css";
 
 function App() {
-
   return (
     <>
-    {/* <Squar_P2_Small/> */}
-    <div className='bg-[#300488]'>
-    <Nav_Bar/>
-    <Page_One/>
-    <Page_Two/>
-
-    </div>
+      <div className="bg-[#300488]">
+        <Nav_Bar />
+        <Page_One />
+        <Page_Two />
+        <Page_Three />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
