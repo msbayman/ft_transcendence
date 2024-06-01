cbeb49914b7d842429ebde3bad5d36a898d8c6e2
ayman <amsaoub@e1r9p14.1337.ma>
2024-06-01 18:22:49 +0100

    login page

--- ft_trans/src/App.tsx
+++ ft_trans/src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Landing_Page from "./components/Landing_Page/Landing_page_com/Landing_Page";
+import Login_Page from "./components/Login_Page/Login_Page";
 
 function App() {
   return (
     <>
-     <Landing_Page/>
+     {/* <Landing_Page/> */}
+     <Login_Page/>
 
     </>
   );
---
+++ ft_trans/src/components/Login_Page/Login_Page.tsx
@@ -0,0 +1,126 @@
+import { TextField, colors } from "@mui/material";
+import "./Login_Page.css";
+
+function Login_Page() {
+  return (
+    <>
+      <div className="main_login">
+        <div className="leftContainer">
+          <div className="login_left">
+            <div className="left_cont">
+              <div className="game_logo_login"></div>
+              <img id="logo_login" src="logo_game.png" alt="game_logo" />
+              <div className="login_form">
+                <div className="div_mail">
+                  <TextField
+                    id="email_f"
+                    label="Email"
+                    maxRows={4}
+                    variant="standard"
+                    sx={{
+                      width: "100%", // Adjust width as needed
+                      "& .MuiInputBase-input": {
+                        color: "white", // Text color
+                        fontSize: "1.25rem", // Adjust font size
+                        backgroundColor: "transparent", // Ensure the input background is transparent
+                      },
+                      "& .MuiInputLabel-root": {
+                        color: "white", // Label color
+                        fontSize: "1.25rem", // Adjust font size for the label
+                      },
+                      "& .MuiInput-underline:before": {
+                        borderBottomColor: "white", // Default underline color
+                      },
+                      "& .MuiInput-underline:hover:not(.Mui-disabled):before": {
+                        borderBottomColor: "white", // Hover underline color
+                      },
+                      "& .MuiInput-underline:after": {
+                        borderBottomColor: "white", // Focused underline color
+                      },
+                      "& .MuiFormHelperText-root": {
+                        color: "white", // Helper text color
+                      },
+                      "& .MuiSvgIcon-root": {
+                        color: "white", // Icon color
+                      },
+                    }}
+                  />
+                </div>
+                <div className="div_pw">
+                  <TextField
+                    id="pw_f"
+                    label="Password"
+                    type="password"
+                    maxRows={4}
+                    variant="standard"
+                    sx={{
+                      width: "100%", // Adjust width as needed
+                      "& .MuiInputBase-input": {
+                        color: "white", // Text color
+                        fontSize: "1.25rem", // Adjust font size
+                        backgroundColor: "transparent", // Ensure the input background is transparent
+                      },
+                      "& .MuiInputLabel-root": {
+                        color: "white", // Label color
+                        fontSize: "1.25rem", // Adjust font size for the label
+                      },
+                      "& .MuiInput-underline:before": {
+                        borderBottomColor: "white", // Default underline color
+                      },
+                      "& .MuiInput-underline:hover:not(.Mui-disabled):before": {
+                        borderBottomColor: "white", // Hover underline color
+                      },
+                      "& .MuiInput-underline:after": {
+                        borderBottomColor: "white", // Focused underline color
+                      },
+                      "& .MuiFormHelperText-root": {
+                        color: "white", // Helper text color
+                      },
+                      "& .MuiSvgIcon-root": {
+                        color: "white", // Icon color
+                      },
+                    }}
+                  />
+                </div>
+
+                <div className="login_btn_forget">
+                  <div className="forget_pass">Forgot Password ?</div>
+                  <button id="btn_login">LOG IN</button>
+                </div>
+                <span className="dotted-line">
+                  <div id="or">or</div>
+                </span>
+              </div>
+              <div className="login_42_google">
+                <img
+                  className="auth"
+                  src="connect_with_google.svg"
+                  alt="login google"
+                />
+                <img
+                  className="auth"
+                  src="connect_with_42.svg"
+                  alt="login intra"
+                />
+              </div>
+
+              <div className="forget_and_singup">
+                <span className="forget_pass">Don't have an account ?</span>
+                <span>Sign Up !</span>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="login_right">
+          <div className="title_login"> WELCOME PLAYER !</div>
+          <div className="img_login_ping">
+            <img src="Login_page/ping pong table.svg" alt="ping pong table" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Login_Page;
