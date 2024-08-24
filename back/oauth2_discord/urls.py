16252dde63dc94fe965e7a769faced62a6c7c02a
msaoub ayman <aymanmsaoub@gmail.com>
2024-08-24 16:24:06 +0100

    i should devide opt from login fun

--- back/oauth2_discord/urls.py
+++ back/oauth2_discord/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 from . import views
-from . import otp_view
 
 urlpatterns = [
     path("login",views.login,name="login"),
     path("login_redirect",views.login_redirect,name="login_redirect"),
-    path("make_req_otp/<str:email_to_send>/", otp_view.send_otp_email, name="make_req_otp"),
+
 ]
