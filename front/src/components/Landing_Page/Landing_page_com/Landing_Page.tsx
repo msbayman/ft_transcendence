40d4671c6598d818aa0856349bbb0585fea56ac9
ayman <aymanmsaoub@gmail.com>
2024-09-10 15:52:11 +0100

    fix navbar landing page

--- front/src/components/Landing_Page/Landing_page_com/Landing_Page.tsx
+++ front/src/components/Landing_Page/Landing_page_com/Landing_Page.tsx
@@ -6,16 +6,14 @@ import Page_Three from "./Page_Three/Page_Three";
 import Sd_Bar_Landing from "./Sd_Bar_Landing/Sd_Bar_Landing";
 
 function Landing_Page() {
-  const [side_bar, setbar] = useState(false);
 
-  const show_hide_sd_bar = () => {
-    setbar(!side_bar);
-  };
+
+ 
 
   return (
     <div className="bg-[#300488]">
-      {side_bar && <Sd_Bar_Landing />}
-      <Nav_Bar show_hide_sd_bar={show_hide_sd_bar} />
+      {/* <Sd_Bar_Landing /> */}
+      <Nav_Bar  />
       <Page_One />
       <Page_Two />
       <Page_Three />
