87846795e4cba453ab04135d072733e7e404f5c5
ayman <aymanmsaoub@gmail.com>
2024-09-12 16:37:21 +0100

    fixed page 2 landing page

--- front/src/components/Landing_Page/Landing_page_com/Sd_Bar_Landing/Sd_Bar_Landing.tsx
+++ front/src/components/Landing_Page/Landing_page_com/Sd_Bar_Landing/Sd_Bar_Landing.tsx
@@ -1,18 +1,45 @@
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import "./Sd_Bar_Landing.css";
 
-import { Link } from 'react-router-dom'
-import './Sd_Bar_Landing.css'
 function Sd_Bar_Landing() {
-    return (
-        <div className='main_div_sid_land'>
-            <ul className='sid_land_list' >
-                <li className='sb_item'><a  href="#l_p_1">Home</a></li>
-                <li className='sb_item'><a  href="#l_p_2">Discover</a></li>
-                <li className='sb_item'><a  href="#l_p_3">About</a></li>
-                <li className='sb_item'> <Link to="login">Login</Link></li>
-                <li className='sb_item'> <Link to="signup">Signup</Link></li>
-            </ul>
-        </div>
-      )
+  const [isVisible, setIsVisible] = useState(true);
+  const handleNavigation = () => {
+    setIsVisible(!isVisible);
+    ;
+  };
+
+  return (
+    <div className={`main_div_sid_land ${!isVisible ? "hide" : ""}`}>
+      <ul className="sid_land_list">
+        <li className="sb_item">
+          <a href="#l_p_1" onClick={() => handleNavigation()}>
+            Home
+          </a>
+        </li>
+        <li className="sb_item">
+          <a href="#l_p_2" onClick={() => handleNavigation()}>
+            Discover
+          </a>
+        </li>
+        <li className="sb_item">
+          <a href="#l_p_3" onClick={() => handleNavigation()}>
+            About
+          </a>
+        </li>
+        <li className="sb_item">
+          <Link to="login" onClick={() => handleNavigation()}>
+            Login
+          </Link>
+        </li>
+        <li className="sb_item">
+          <Link to="signup" onClick={() => handleNavigation()}>
+            Signup
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
-export default Sd_Bar_Landing
+export default Sd_Bar_Landing;
