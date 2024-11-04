7b80a88a4a74bdaf44955c8c38309268c559a828
Abdellah El Kacem <ael-kace@e2r10p15.1337.ma>
2024-11-04 23:39:23 +0100

    almost finish view of Overview

--- front/src/pages/My_Profile/Overview_Page/Overview_Page.tsx
+++ front/src/pages/My_Profile/Overview_Page/Overview_Page.tsx
@@ -1,5 +1,21 @@
-import React from 'react'
+// import React from "react";
+import "./Overview_Page.css";
 
-export const Overview_Page = () => <div>Overviggggew</div>;
+export const Overview_Page = () => {
+  return (
+    <div className="Overview_Page">
+      <div className="Part_1">
+        <div className="Background_Profile"></div>
+        <div className="States_Profile"></div>
+        <div className="Top_Achievement"></div>
+      </div>
+      <div className="Part_2">
+        <div className="Leaderboard"></div>
+        <div className="Online_Friends"></div>
+        <div className="Play_Button"></div>
+      </div>
+    </div>
+  );
+};
 
-export default Overview_Page
+export default Overview_Page;
