3fc3f464eb956fcc1c3e68196528f2d6ccd843a0
Abdellah El Kacem <ael-kace@e2r3p14.1337.ma>
2024-11-15 22:12:06 +0100

    almost finish leaderboard

--- front/src/pages/My_Profile/Overview_Page/Leaderboard.tsx
+++ front/src/pages/My_Profile/Overview_Page/Leaderboard.tsx
@@ -1,11 +1,50 @@
-import React from "react";
+// import React from "react";
 import "./Leaderboard.css";
 
 const Leaderboard = () => {
   return (
     <div className="All_Content_Leaderboard">
-      <div className="Title_Leaderboard"></div>
-      <div>
+      <div className="Title_Leaderboard">
+        <div className="The_Title_Leaderboard">Leaderboard</div>
+        <div className="Button_More">More</div>
+      </div>
+      <div className="Table_Leaderboard">
+        <div className="Title_Table_Leaderboard">
+          <div className="the_Name Rank">Rank</div>
+          <div className="the_Name UserName"> UserName</div>
+          <div className="the_Name Top10">Top 10</div>
+          <div className="the_Name Score">Score</div>
+        </div>
+        <div className="Inside_Leaderboard">
+          <p>sdfsdfdsfdsf</p>
+          <p>sdfsdfdsfdsf</p>
+          <p>sdfsdfdsfdsf</p>
+          <p>sdfsdfdsfdsf</p>
+          <p>sdfsdfdsfdsf</p>
+          <p>sdfsdfdsfdsf</p>
+          <p>sdfsdfdsfdsf</p>
+          <p>sdfsdfdsfdsf</p>
+          <p>sdfsdfdsfdsf</p>
+          <p>sdfsdfdsfdsf</p>
+          <p>sdfsdfdsfdsf</p>
+          <p>sdfsdfdsfdsf</p>
+          <p>sdfsdfdsfdsf</p>
+          <p>sdfsdfdsfdsf</p>
+          <p>sdfsdfdsfdsf</p>
+          <p>sdfsdfdsfdsf</p>
+          <p>sdfsdfdsfdsf</p>
+          <p>sdfsdfdsfdsf</p>
+          <p>sdfsdfdsfdsf</p>
+          <p>sdfsdfdsfdsf</p>
+          <p>sdfsdfdsfdsf</p>
+          <p>sdfsdfdsfdsf</p>
+          <p>sdfsdfdsfdsf</p>
+          <p>sdfsdfdsfdsf</p>
+          <p>sdfsdfdsfdsf</p>
+          <p>sdfsdfdsfdsf</p>
+          <p>sdfsdfdsfdsf</p>
+          <p>sdfsdfdsfdsf</p>
+        </div>
       </div>
     </div>
   );
