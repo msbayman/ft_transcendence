4e783b44eafcfaae4f3e7890693a1451b928603b
ayman <aymanmsaoub@gmail.com>
2024-11-19 18:00:46 +0100

    i can use Oauth in sing up page

--- front/src/pages/Signup_Page/Signup_Page.tsx
+++ front/src/pages/Signup_Page/Signup_Page.tsx
@@ -81,6 +81,12 @@ function Signup_Page() {
       }
     }
   };
+  const handleOAuthLogin = () => {
+    window.location.href = "http://localhost:8000/discord/login";
+  };
+  const handleOAuthLogin_42 = () => {
+    window.location.href = "http://localhost:8000/42/login";
+  };
 
   return (
     <div className="main_signup">
@@ -102,12 +108,14 @@ function Signup_Page() {
             </Link>
             <form className="signup_form" onSubmit={handleSubmit(onSubmit)}>
               <img
-                className="auth"
+                className="auth cursor-pointer"
+                onClick={handleOAuthLogin}
                 src="connect_with_google.svg"
                 alt="login google"
               />
               <img
-                className="auth"
+                className="auth cursor-pointer"
+                onClick={handleOAuthLogin_42}
                 src="connect_with_42.svg"
                 alt="login intra"
               />
