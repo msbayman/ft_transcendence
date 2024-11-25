3709a605d112def9d1d3dc1d28e7e6d33de02178
sakarkal <karkalsaad@gmail.com>
2024-11-25 21:53:49 +0100

    Settings Page: 2st password component, without responsive

---
+++ front/src/pages/My_Profile/Settings_Page/Password_component.tsx
@@ -0,0 +1,23 @@
+
+function Password_component() {
+  return (
+	<div className='pswd'>
+		<div className='inpt'>
+			<span className='spn'>
+				<label htmlFor='1'>Current Password</label>
+				<input type="text" placeholder='Current Password' id='1'/>
+			</span>
+			<span className='spn'>
+				<label>New Password</label>
+				<input type="text" placeholder='New Password' />
+			</span>
+			<span className='spn'>
+				<label>Confirme New Password</label>
+				<input type="text" placeholder='Confirme New Password'/>
+			</span>
+		</div>
+	</div>
+  )
+}
+
+export default Password_component
