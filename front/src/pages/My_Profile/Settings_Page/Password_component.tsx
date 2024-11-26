3c47f37a5e23993bdef6af148b9ea69b37514759
sakarkal <karkalsaad@gmail.com>
2024-11-26 20:41:56 +0100

    Settings Page: 2st password component, Adding icon and 9aditom 3la wedit show/hide password

--- front/src/pages/My_Profile/Settings_Page/Password_component.tsx
+++ front/src/pages/My_Profile/Settings_Page/Password_component.tsx
@@ -1,19 +1,47 @@
+import { BsEyeFill , BsEyeSlashFill } from "react-icons/bs";
+import { useState } from "react";
+
 
 function Password_component() {
+
+	const [passwordVisible, setPasswordVisible] = useState({
+		currentPassword: false,
+		newPassword: false,
+		confirmPassword: false,
+	});
+
+	const toggleVisibility = (field: string) => {
+		console.log("heeere");
+		setPasswordVisible((prev) => ({
+			...prev,
+			[field]: !prev[field],
+		}));
+	};
+	
+
   return (
 	<div className='pswd'>
 		<div className='inpt'>
 			<span className='spn'>
-				<label htmlFor='1'>Current Password</label>
-				<input type="password" placeholder='Current Password' id='1'/>
+				<label>Current Password</label>
+					<i className="icon" onClick={() => toggleVisibility("currentPassword")}>
+						{passwordVisible.currentPassword ? <BsEyeFill /> : <BsEyeSlashFill />}
+					</i>
+				<input type={passwordVisible.currentPassword ? "text" : "password"} placeholder='Current Password' className="pass" />
 			</span>
 			<span className='spn'>
 				<label>New Password</label>
-				<input type="password" placeholder='New Password' />
+					<i className="icon" onClick={() => toggleVisibility("newPassword")}>
+						{passwordVisible.newPassword ? <BsEyeFill /> : <BsEyeSlashFill />}
+					</i>
+				<input type={passwordVisible.newPassword ? "text" : "password"} placeholder='New Password' className="pass" />
 			</span>
 			<span className='spn'>
 				<label>Confirme New Password</label>
-				<input type="password" placeholder='Confirme New Password'/>
+					<i className="icon" onClick={() => toggleVisibility("confirmPassword")}>
+						{passwordVisible.confirmPassword ? <BsEyeFill /> : <BsEyeSlashFill />}
+					</i>
+				<input type={passwordVisible.confirmPassword ? "text" : "password"} placeholder='Confirme New Password' className="pass" />
 			</span>
 		</div>
 	</div>
