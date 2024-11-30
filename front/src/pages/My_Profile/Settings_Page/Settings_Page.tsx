dba5f493ea8aee50cea9320ca49fef36fa82db85
sakarkal <karkalsaad@gmail.com>
2024-11-30 21:13:31 +0100

    Settings Page: centring some divs in profile settings

--- front/src/pages/My_Profile/Settings_Page/Settings_Page.tsx
+++ front/src/pages/My_Profile/Settings_Page/Settings_Page.tsx
@@ -1,52 +1,50 @@
-import { useState } from 'react'
-import './Settings_Page.css'
-import Security_box from './Security_box';
-import Profile_side from './Profile_side'
+import { useState } from "react";
+import "./Settings_Page.css";
+import Security_box from "./Security_box";
+import Profile_side from "./Profile_side";
 
 const Settings_Page = () => {
-  const [action, setAction] = useState('');
+  const [action, setAction] = useState("");
 
   const Profile = () => {
-    setAction(' active');
-	};
-  
-	const Security = () => {
-    setAction('');
-	};
+    setAction(" active");
+  };
 
+  const Security = () => {
+    setAction("");
+  };
 
   return (
     <div className="wrapper">
       <h1>Settings</h1>
 
-
-      <div className='options'>
+      <div className="options">
         <div className={`abs${action}`} />
-        <div className='prfl'>
-          <a href="#" onClick={Security}>Profile</a>
+        <div className="prfl">
+          <a href="#" onClick={Security}>
+            Profile
+          </a>
         </div>
-        <div className='sec'>
-          <a href="#" onClick={Profile}>Security</a>
+        <div className="sec">
+          <a href="#" onClick={Profile}>
+            Security
+          </a>
         </div>
       </div>
 
-      <div className='content'>
-        <Profile_side /> 
+      <div className="content">
+        <Profile_side />
         {/* <Security_box /> */}
       </div>
 
-
-      <div className='save-cancel'>
-        <div>
-          <button className='btn cancel'>Cancel</button>
-        </div>
-        <div>
-          <button className='btn save'>Save</button>
+      <div className="save-cancel">
+        <div className="child-btn">
+          <button className="btn cancel">Cancel</button>
+          <button className="btn save">Save</button>
         </div>
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default Settings_Page
+export default Settings_Page;
