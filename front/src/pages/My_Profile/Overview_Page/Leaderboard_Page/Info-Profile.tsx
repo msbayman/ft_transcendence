3fc6a91d4fe463282f7439c0f7d6ef785028c0a0
Abdellah El Kacem <ael-kace@e2r3p14.1337.ma>
2024-12-18 06:39:55 +0100

    add leaderboard page

---
+++ front/src/pages/My_Profile/Overview_Page/Leaderboard_Page/Info-Profile.tsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import "./Info-Profile.css"
+import ph_pro from "../../Images/profile.png"
+import etoile from "../../Images/Etoile.svg"
+
+const Info_Profile = () => {
+  return (
+    <>
+      <div className="part11">
+        <div className="Score_style font-text">Your Score</div>
+        <div className="Score_style_value">850 Points</div>
+        <div className="Photo_dProfile">
+          <img src={ph_pro} className="Photo_dProfile" />
+        </div>
+        <div className='Name_of_Profile'>KACIMO</div>
+        <div className="The_level">
+          <div className="level_Profile">
+            <img src={etoile} className='Etoile_lvl'/>
+            <span>3</span>
+          </div>
+          <div className="Exp_Level">125 OF 365</div>
+        </div>
+      </div>
+      <div className="part22">
+        <div className="title_static">
+          Game Statistics <hr />
+        </div>
+        <div className="Static_Game">
+          <ul>
+            <li className="Static_Text">
+              <span className="Color_Yellow">GAMES WON:</span> 6 OF 8
+            </li>
+            <li className="Static_Text">
+              <span className="Color_Yellow">WIN PERCENTAGE:</span> 80%
+            </li>
+            <li className="Static_Text">
+              <span className="Color_Yellow">RANK:</span> BRONZE
+            </li>
+            <li className="Static_Text">
+              <span className="Color_Yellow">WIN STREAK:</span> 4
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Info_Profile
