ed8e46a6cc0534646d5e66e7aab1c7737f3dbb2a
sakarkal <karkalsaad@gmail.com>
2024-12-19 20:58:53 +0100

    just something about fuild in tailwind

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -1,11 +1,18 @@
+import fluid, { extract, screens, fontSize } from 'fluid-tailwind'
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
+  content: {
+    files: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+    "./src/**/*.{js,ts,jsx,tsx}"],
+    extract,
+},
   theme: {
+    screens,
+    fontSize,    
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    fluid
+  ],
 }
