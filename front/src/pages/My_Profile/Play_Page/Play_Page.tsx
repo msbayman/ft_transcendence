00880d1e82d6d5aae906946aee0422ff23090c31
sakarkal <karkalsaad@gmail.com>
2024-12-19 20:58:23 +0100

    Play Page: preparing modes part

--- front/src/pages/My_Profile/Play_Page/Play_Page.tsx
+++ front/src/pages/My_Profile/Play_Page/Play_Page.tsx
@@ -10,23 +10,24 @@ import Bot from "../assets/Bot.svg"
 const Play_Page = () => {
   return (
     <div className='main'>
-      <div>
-        <h1>Play</h1>
+      <div className='utils'>
+        
+        <h1 className='bg-sky-500 ~px-4/8 ~py-2/4 ~text-sm/3xl ' >Issam</h1>
       </div>
-      <div className='relative grid grid-flow-col grid-rows-2 grid-cols-2 gap-10'>
-        <div className='modes'>
+      <div className='here relative grid grid-cols-1 lg:grid-cols-2 gap-10 border'>
+        <div className='modes ~sm/lg'>
           <img className='image' src={OneVsOne} />
-          <p className='one'>1 V 1</p>
+          <p className='one '>1 V 1</p>
         </div>
-        <div className='modes'>
-          <img className='image' src={Bot} />
-          <p className='bot'>VS BOT</p>
-        </div>
-        <div className='modes'>
+        <div className='modes ~sm/lg'>
           <img className='image' src={TwoVsTwo} />
           <p className='two'>2 V 2</p>
         </div>
-        <div className='modes'>
+        <div className='modes ~sm/lg'>
+          <img className='image' src={Bot} />
+          <p className='bot'>VS BOT</p>
+        </div>
+        <div className='modes ~sm/lg'>
           <img className='image' src={Tournemant} />
           <p className='tourn'>TOURN</p>
         </div>
