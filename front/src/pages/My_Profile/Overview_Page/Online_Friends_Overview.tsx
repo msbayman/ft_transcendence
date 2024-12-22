15a07a38cae99670312b1b0e1f7bea0a9b0ec988
Abdellah El Kacem <ael-kace@e1r4p12.1337.ma>
2024-12-22 14:37:50 +0100

    add button message and play in overview

--- front/src/pages/My_Profile/Overview_Page/Online_Friends_Overview.tsx
+++ front/src/pages/My_Profile/Overview_Page/Online_Friends_Overview.tsx
@@ -1,20 +1,27 @@
 import React from 'react'
 import "./Online_Friends_Overview.css"
 import users from './Fake_Table.json'
+import Message from '../Images/Message_to_User.svg'
+import Invite_Play from '../Images/Invite_to_play.svg'
+import { useNavigate } from 'react-router-dom'
 
 interface User {
   _id: string;
   User: string;
   Path_Image: string;
-  Online: string; // Stored as string in JSON
+  Online: string;
   score: string;
 }
 
 const Online_Friends_Overview = () => {
+  const navgate = useNavigate();
+  const to_message = () => {
+    navgate("/Friends")
+  }
+  const to_play = () => {
+    navgate("/Play")
+  }
   const onlineFriends = users.filter((user: User) => user.Online === "true");
-  const extractIdNumber = (_id: string): number => {
-    return parseInt(_id.replace("#", ""), 10);
-  };
   return (
     <div className="all_content_Online">
       <div className="Title_Onlin">
@@ -29,6 +36,20 @@ const Online_Friends_Overview = () => {
             <span className='online_cercel'></span>
             </div>
             <span className='User_name'>{friend.User}</span>
+            <div className='click'>
+              <div className='hove_contain'>
+                <button onClick={to_message}><img src={Message} className='img_siz'/>
+                  <span className='hove'>Message</span>
+                </button>
+                </div>
+              <div className='hove_contain'>
+                <button onClick={to_play}><img src={Invite_Play} className='img_siz' />
+                  <span className='hove'>Challenge</span>
+                </button>
+              </div>
+              
+              
+            </div>
           </li>
         ))}
       </ul>
