0b069281d51d53c6891ae8aeab628a83092ab800
Saad Karkal <karkalsaad@gmail.com>
2024-12-25 00:47:56 +0100

    some changes khaserhom liya linux o 3awed 9adithom mel mac f E2

--- front/src/pages/My_Profile/Play_Page/Play_Page.tsx
+++ front/src/pages/My_Profile/Play_Page/Play_Page.tsx
@@ -2,34 +2,32 @@
 import './Play_Page.css'
 import OneVsOne from "../assets/1v1.svg"
 import TwoVsTwo from "../assets/2v2.svg"
-import Tournemant from "../assets/tourn.svg"
-import Bot from "../assets/Bot.svg"
+import Tournemant from "../assets/Tourn.svg"
+import Bot from "../assets/vsBot.svg"
 
 
 
 const Play_Page = () => {
   return (
     <div className='main'>
-      <div className='utils'>
-        
-        <h1 className='bg-sky-500 ~px-4/8 ~py-2/4 ~text-sm/3xl ' >Issam</h1>
+      <div className='utils relative'>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
       </div>
-      <div className='here relative grid grid-cols-1 lg:grid-cols-2 gap-10 border'>
+      <div className='here relative grid grid-cols-1 lg:grid-cols-2 gap-10 bottom-5'>
         <div className='modes ~sm/lg'>
           <img className='image' src={OneVsOne} />
-          <p className='one '>1 V 1</p>
         </div>
         <div className='modes ~sm/lg'>
           <img className='image' src={TwoVsTwo} />
-          <p className='two'>2 V 2</p>
         </div>
         <div className='modes ~sm/lg'>
           <img className='image' src={Bot} />
-          <p className='bot'>VS BOT</p>
         </div>
         <div className='modes ~sm/lg'>
           <img className='image' src={Tournemant} />
-          <p className='tourn'>TOURN</p>
         </div>
       </div>
     </div>
--- front/src/pages/My_Profile/Settings_Page/TwoFA-component.tsx
+++ front/src/pages/My_Profile/Settings_Page/TwoFA-component.tsx
@@ -1,6 +1,51 @@
 import './Settings_Page.css'
+import { useState } from 'react'
+
+
 
 function TwoFA_Component() {
+
+
+	const [activated, setActivated] = useState(false);
+// 	const [showModal, setShowModal] = useState(false);
+// 	const [otp, setOtp] = useState(["", "", "", "", "", ""]);
+
+	const handleClick = () => {
+		if (!activated) {
+			// setShowModal(true);
+			console.log("2FA activated");
+		} else {
+			setActivated(false);
+			console.log("2FA deactivated");
+		}
+	};
+
+// 	const handleOtpChange = (index: any, value : any) => {
+// 		if (/\d/.test(value) || value === "") {
+// 			const newOtp = [...otp];
+// 			newOtp[index] = value;
+// 			setOtp(newOtp);
+// 			if (value !== "" && index < 5) {
+// 				document.getElementById(`otp-${index + 1}`)?.focus(); // Auto-focus next input
+// 			}
+// 		}
+// 	};
+
+// 	const handleVerify = () => {
+// 		const isComplete = otp.every((digit) => digit !== "");
+// 		if (isComplete) {
+// 			setActivated(true);
+// 			setShowModal(false);
+// 			console.log("2FA activated");
+// 		} else {
+// 			alert("Please complete the OTP.");
+// 		}
+// 	};
+
+//   const handleResend = () => {
+//     console.log("Resend OTP");
+//   };
+
   return (
 	<>
 	<h1 className='text-container'>Two-Factor Authentication (2FA)</h1>    
@@ -9,7 +54,35 @@ function TwoFA_Component() {
 			This feature adds an extra of protection, ensuring that only you can access.
 			Your account, even if your password is compromised</p>
 		<div className='check-TFA'>
-			<input type="checkbox" id="twoFAChoice1" name="" value="email" />
+			<input type="checkbox" id="twoFAChoice1" name="" value="email" onClick={handleClick}/>
+			{/* {
+			showModal && (
+				<div className="modal">
+				<div className="modal-content">
+					<h3>Enter OTP</h3>
+					<div className="otp-inputs">
+					{otp.map((digit, index) => (
+						<input
+						key={index}
+						id={`otp-${index}`}
+						type="text"
+						maxLength={1}
+						value={digit}
+						onChange={(e) => handleOtpChange(index, e.target.value)}
+						/>
+					))}
+					</div>
+					<p>
+					Don't get OTP? <span className="resend" onClick={handleResend}>Resend Code</span>
+					</p>
+					<div className="modal-buttons">
+					<button onClick={() => setShowModal(false)}>Cancel</button>
+					<button onClick={handleVerify}>Verify</button>
+					</div>
+				</div>
+				</div>
+			)
+	  	} */}
 			<label>Activate Two-factor authentication (2FA)</label>
 		</div>
 	</div>
@@ -18,3 +91,4 @@ function TwoFA_Component() {
 }
 
 export default TwoFA_Component
+
