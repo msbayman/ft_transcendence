49781c4140cb99258524a3f67be175b45d2a5962
Saad Karkal <karkalsaad@gmail.com>
2025-01-13 02:35:04 +0100

    chore: Update Play_Page styles and add Buttons component

--- front/src/pages/My_Profile/Play_Page/Options.tsx
+++ front/src/pages/My_Profile/Play_Page/Options.tsx
@@ -1,32 +1,33 @@
+
 function Options() {
 
   return (
     <div className="absolute top-[5%] z-10  w-[80%] h-[10%] bg-[#3A0CA3] rounded-[65px] shadow-2xl flex justify-evenly gap-10 items-center">
 
-      <h1 className="flex justify-center items-center flex-col-reverse">
+      <span className="flex justify-center items-center flex-col-reverse">
         <h4 className="text-white text-2xl font-alexandria">MODES</h4>
-        <img src="mode.svg" alt="Modes" className="animate-pulse " />
-      </h1>
+        <img src="mode.svg" alt="Modes"/>
+      </span>
 
-      <h1 className="flex justify-center items-center flex-col-reverse">
+      <span className="flex justify-center items-center flex-col-reverse">
         <h4 className="text-white text-2xl font-alexandria">BOARDS</h4>
-        <img src="Board.svg" alt="Modes" className="animate-pulse " />
-      </h1>
+        <img src="Board.svg" alt="Modes"/>
+      </span>
 
-      <h1 className="flex justify-center items-center flex-col-reverse">
+      <span className="flex justify-center items-center flex-col-reverse">
         <h4 className="text-white text-2xl font-alexandria">CUES</h4>
-        <img src="cue.svg" alt="Modes" className="animate-pulse " />
-      </h1>
+        <img src="cue.svg" alt="Modes"/>
+      </span>
 
-      <h1 className="flex justify-center items-center flex-col-reverse">
+      <span className="flex justify-center items-center flex-col-reverse">
         <h4 className="text-white text-2xl font-alexandria">BALLS</h4>
-        <img src="ball.svg" alt="Modes" className="animate-pulse " />
-      </h1>
+        <img src="ball.svg" alt="Modes"/>
+      </span>
 
-      <h1 className="flex justify-center items-center flex-col-reverse">
+      <span className="flex justify-center items-center flex-col-reverse">
         <h4 className="text-white text-2xl font-alexandria">FINISH</h4>
-        <img src="finish.svg" alt="Modes" className="animate-pulse " />
-      </h1>
+        <img src="finish.svg" alt="Modes"/>
+      </span>
 
     </div>
   );
--- front/src/pages/My_Profile/Play_Page/Play_Page.tsx
+++ front/src/pages/My_Profile/Play_Page/Play_Page.tsx
@@ -1,54 +1,95 @@
-import React from "react";
 import "./Play_Page.css";
 import EmblaCarousel from "./EmblaCarousel/EmblaCarousel";
 import { EmblaOptionsType } from "embla-carousel";
 import Options from "./Options";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom"; // Import if using react-router
+import { NextButton, PrevButton } from "./Buttons";
+import classes from "./style.module.css";
 
 interface SelectedIds {
   mode: number | null;
   board: number | null;
   paddel: number | null;
   ball: number | null;
+  selectedStatus: {
+    mode: number[];
+    board: number[];
+    paddel: number[];
+    ball: number[];
+  };
 }
-
 const OPTIONS: EmblaOptionsType = { loop: true };
 
 const SLIDEIMAPS = [
-  { mapPath: "1v1.png", id: 0 },
-  { mapPath: "2v2.png", id: 1 },
-  { mapPath: "vsBot.png", id: 2 },
-  { mapPath: "Tourn.png", id: 3 },
+  { mapPath: "1v1.png", id: 0, mapName: "1v1" },
+  { mapPath: "2v2.png", id: 1, mapName: "2v2" },
+  { mapPath: "vsBot.png", id: 2, mapName: "vsBot" },
+  { mapPath: "Tourn.png", id: 3, mapName: "Tournement" },
 ];
 
 const SLIDECUES = [
-  { mapPath: "Cues/bluePoolCue.svg", id: 0 },
-  { mapPath: "Cues/brownPoolCue.svg", id: 1 },
-  { mapPath: "Cues/greenNeonCue.svg", id: 2 },
-  { mapPath: "Cues/pinkNeonCue.svg", id: 3 },
+  {
+    mapPath: "skins-svg/Paddel/BluePool-Cue.svg",
+    id: 0,
+    mapName: "BluePool-Cue",
+  },
+  {
+    mapPath: "skins-svg/Paddel/GreenNeonCue.svg",
+    id: 1,
+    mapName: "GreenNeonCue",
+  },
+  {
+    mapPath: "skins-svg/Paddel/PinkNeonCue.svg",
+    id: 2,
+    mapName: "PinkNeonCue",
+  },
+  {
+    mapPath: "skins-svg/Paddel/BrownPool-Cue.svg",
+    id: 3,
+    mapName: "BrownPool-Cue",
+  },
 ];
 
 const SLIDEBALLS = [
-  { mapPath: "Balls/8BallPool.svg", id: 0 },
-  { mapPath: "Balls/FootBall.svg", id: 1 },
-  { mapPath: "Balls/BasketBall.svg", id: 2 },
+  { mapPath: "skins-png/Balls/8BallPool.png", id: 0, mapName: "8BallPool" },
+  { mapPath: "skins-png/Balls/Foot-Ball.png", id: 1, mapName: "FootBall" },
+  { mapPath: "skins-png/Balls/Basket-Ball.png", id: 2, mapName: "BasketBall" },
 ];
 
 const SLIDEBOARDS = [
-  { mapPath: "boards/blueBoard.svg", id: 0 },
-  { mapPath: "boards/footballBoard.svg", id: 1 },
-  { mapPath: "boards/greenBoard.svg", id: 2 },
+  {
+    mapPath: "skins-svg/Boards/FootBall.svg",
+    id: 0,
+    mapName: "FootBall-Board",
+  },
+  {
+    mapPath: "skins-svg/Boards/BlueBoard.svg",
+    id: 1,
+    mapName: "BlueBoard-Board",
+  },
+  {
+    mapPath: "skins-svg/Boards/GreenBoard.svg",
+    id: 2,
+    mapName: "GreenBoard-Board",
+  },
+  { mapPath: "skins-svg/Boards/FootBall.svg", id: 3, mapName: "-Board" },
 ];
 
 const Play_Page: React.FC = () => {
-  const navigate = useNavigate(); // If using react-router
+  const navigate = useNavigate(); // If using react-router for navigation
   const [value, setValue] = useState("Modes");
   const [selectedIds, setSelectedIds] = useState<SelectedIds>({
     mode: null,
     board: null,
     paddel: null,
     ball: null,
+    selectedStatus: {
+      mode: [],
+      board: [],
+      paddel: [],
+      ball: [],
+    },
   });
   const [currentSlideIndex, setCurrentSlideIndex] = useState<number>(0);
 
@@ -75,7 +116,30 @@ const Play_Page: React.FC = () => {
     }
   };
 
+  ///////
+  // const [selected, setSelected] = useState<boolean>(false);
+  // useEffect(() => {
+  //   setSelected(false);
+  // }, [currentSlideIndex]);
+  ///////
+
+  const isCurrentSlideSelected = () => {
+    switch (value) {
+      case "Modes":
+        return selectedIds.selectedStatus.mode.includes(currentSlideIndex);
+      case "Boards":
+        return selectedIds.selectedStatus.board.includes(currentSlideIndex);
+      case "Paddels":
+        return selectedIds.selectedStatus.paddel.includes(currentSlideIndex);
+      case "Ball":
+        return selectedIds.selectedStatus.ball.includes(currentSlideIndex);
+      default:
+        return false;
+    }
+  };
+
   const handleNextClick = () => {
+    // setSelected(false);
     switch (value) {
       case "Modes":
         setValue("Boards");
@@ -99,23 +163,65 @@ const Play_Page: React.FC = () => {
         break;
     }
   };
-
   const handleSelect = () => {
     switch (value) {
       case "Modes":
-        setSelectedIds((prev) => ({ ...prev, mode: currentSlideIndex }));
+        setSelectedIds((prev) => ({
+          ...prev,
+          mode: currentSlideIndex,
+          selectedStatus: {
+            ...prev.selectedStatus,
+            mode: isCurrentSlideSelected()
+              ? prev.selectedStatus.mode.filter(
+                  (id) => id !== currentSlideIndex
+                )
+              : [...prev.selectedStatus.mode, currentSlideIndex],
+          },
+        }));
         break;
       case "Boards":
-        setSelectedIds((prev) => ({ ...prev, board: currentSlideIndex }));
+        setSelectedIds((prev) => ({
+          ...prev,
+          board: currentSlideIndex,
+          selectedStatus: {
+            ...prev.selectedStatus,
+            board: isCurrentSlideSelected()
+              ? prev.selectedStatus.board.filter(
+                  (id) => id !== currentSlideIndex
+                )
+              : [...prev.selectedStatus.board, currentSlideIndex],
+          },
+        }));
         break;
       case "Paddels":
-        setSelectedIds((prev) => ({ ...prev, paddel: currentSlideIndex }));
+        setSelectedIds((prev) => ({
+          ...prev,
+          paddel: currentSlideIndex,
+          selectedStatus: {
+            ...prev.selectedStatus,
+            paddel: isCurrentSlideSelected()
+              ? prev.selectedStatus.paddel.filter(
+                  (id) => id !== currentSlideIndex
+                )
+              : [...prev.selectedStatus.paddel, currentSlideIndex],
+          },
+        }));
         break;
       case "Ball":
-        setSelectedIds((prev) => ({ ...prev, ball: currentSlideIndex }));
+        setSelectedIds((prev) => ({
+          ...prev,
+          ball: currentSlideIndex,
+          selectedStatus: {
+            ...prev.selectedStatus,
+            ball: isCurrentSlideSelected()
+              ? prev.selectedStatus.ball.filter(
+                  (id) => id !== currentSlideIndex
+                )
+              : [...prev.selectedStatus.ball, currentSlideIndex],
+          },
+        }));
         break;
     }
-    // Save to localStorage
     localStorage.setItem("selectedSkins", JSON.stringify(selectedIds));
   };
 
@@ -126,86 +232,116 @@ const Play_Page: React.FC = () => {
     navigate("/game", { state: { selectedIds } });
   };
 
+  console.log({ currentSlideIndex });
   return (
-    <main className="overflow-scroll scrollbar-hide min-h-[941px] max-w-[1550px] relative flex justify-center items-center flex-wrap h-auto w-full md:m-10 m-0  rounded-3xl ">
-      <Options />
-      <div className="absolute flex flex-wrap justify-evenly items-center  h-[90%] bottom-0  w-full  overflow-scroll scrollbar-hide bg-[#3A0CA3] rounded-[6rem] shadow-2xl">
-        {value === "Modes" && (
-          <EmblaCarousel
-            slidesmaps={SLIDEIMAPS}
-            options={OPTIONS}
-            setCurrentSlideIndex={setCurrentSlideIndex}
-          />
-        )}
-        {value === "Boards" && (
-          <EmblaCarousel
-            slidesmaps={SLIDEBOARDS}
-            options={OPTIONS}
-            setCurrentSlideIndex={setCurrentSlideIndex}
-          />
-        )}
-        {value === "Paddels" && (
-          <EmblaCarousel
-            slidesmaps={SLIDECUES}
-            options={OPTIONS}
-            setCurrentSlideIndex={setCurrentSlideIndex}
-          />
-        )}
-        {value === "Ball" && (
-          <EmblaCarousel
-            slidesmaps={SLIDEBALLS}
-            options={OPTIONS}
-            setCurrentSlideIndex={setCurrentSlideIndex}
-          />
-        )}
-        {value === "Finish" && (
-          <div className="grid grid-flow-row  gap-4">
-            <img
-              src={SLIDEIMAPS[selectedIds.mode!]?.mapPath}
-              alt={SLIDEIMAPS[selectedIds.mode!]?.mapPath}
+    <main className="overflow-scroll scrollbar-hide min-h-[941px] max-w-[1550px] relative flex justify-center items-baseline flex-wrap h-auto w-full md:m-10 m-0  rounded-3xl ">
+      <Options /> {/* Options component value={value}  */}
+      <div className="absolute flex flex-col flex-wrap justify-evenly items-center h-[90%] bottom-0  w-full  overflow-scroll scrollbar-hide bg-[#3A0CA3] rounded-[6rem] shadow-lg">
+        <div className=" flex-1 h-full  flex flex-col justify-center items-center">
+          {value === "Modes" && (
+            <EmblaCarousel
+              slidesmaps={SLIDEIMAPS}
+              options={OPTIONS}
+              setCurrentSlideIndex={setCurrentSlideIndex}
             />
-            <img
-              src={SLIDEBOARDS[selectedIds.board!]?.mapPath}
-              alt={SLIDEBOARDS[selectedIds.board!]?.mapPath}
+          )}
+          {value === "Boards" && (
+            <EmblaCarousel
+              slidesmaps={SLIDEBOARDS}
+              options={OPTIONS}
+              setCurrentSlideIndex={setCurrentSlideIndex}
             />
-            <img
-              src={SLIDECUES[selectedIds.paddel!]?.mapPath}
-              alt={SLIDECUES[selectedIds.paddel!]?.mapPath}
+          )}
+          {value === "Paddels" && (
+            <EmblaCarousel
+              slidesmaps={SLIDECUES}
+              options={OPTIONS}
+              setCurrentSlideIndex={setCurrentSlideIndex}
             />
-            <img
-              src={SLIDEBALLS[selectedIds.ball!]?.mapPath}
-              alt={SLIDEBALLS[selectedIds.ball!]?.mapPath}
+          )}
+          {value === "Ball" && (
+            <EmblaCarousel
+              slidesmaps={SLIDEBALLS}
+              options={OPTIONS}
+              setCurrentSlideIndex={setCurrentSlideIndex}
             />
-          </div>
-        )}
+          )}
+          {value === "Finish" && (
+            <>
+              <div className="w-full">
+                <img
+                  src={SLIDEIMAPS[selectedIds.mode!]?.mapPath}
+                  alt={SLIDEIMAPS[selectedIds.mode!]?.mapPath}
+                  className="relative flex justify-center items-center right-[1rem] w-full h-[22rem] object-contain"
+                />
+              </div>
 
-        {value === "Modes" ? (
-          <></>
-        ) : (
+              <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+                {/* Football Board */}
+                <img
+                  src={SLIDEBOARDS[selectedIds.board!]?.mapPath}
+                  alt="Football Board"
+                  className="w-full h-[12rem] object-contain"
+                />
+
+                <img
+                  src={SLIDECUES[selectedIds.paddel!]?.mapPath}
+                  alt="Green Neon Cue"
+                  className="w-full h-[12rem] object-contain"
+                />
+
+                <img
+                  src={SLIDEBALLS[selectedIds.ball!]?.mapPath}
+                  alt="Football Ball"
+                  className="w-full h-[12rem] object-contain"
+                />
+              </div>
+            </>
+          )}
+        </div>
+
+        <div className="absolute bottom-11 w-full h-[10%] flex justify-evenly items-center">
           <button
-            className="bg-yellow-500 px-4 py-2 rounded-lg text-white"
+            className="rounded-full text-white border bg-[#3A0CA3] hover:bg-white hover:text-[#3A0CA3] transition-all duration-400 group"
+            style={{ visibility: bool ? "hidden" : "visible" }}
             onClick={handlePrevClick}
-            disabled={bool}
           >
-            &laquo; Back
+            <PrevButton />
+            <span className="hidden opacity-0 absolute transform  bg-black text-white px-2.5 py-1 rounded whitespace-nowrap transition-opacity duration-200 group-hover:block group-hover:opacity-100">
+              PREV
+            </span>
           </button>
-        )}
 
-        {value !== "Finish" && (
-          <button
-            className="bg-green-600 px-4 py-2 rounded-lg text-white"
-            onClick={handleSelect}
-          >
-            Select
-          </button>
-        )}
-
-        <button
-          className="bg-yellow-500 px-4 py-2 rounded-lg text-white"
-          onClick={value !== "Finish" ? handleNextClick : handlePlayClick}
-        >
-          {value === "Finish" ? "Play" : "Next"} &raquo;
-        </button>
+          {value !== "Finish" && (
+            <button
+              className={
+                isCurrentSlideSelected() ? classes.selected : classes.select
+              }
+              onClick={handleSelect}
+            >
+              {isCurrentSlideSelected() ? "SELECTED" : "SELECT"}
+            </button>
+          )}
+          {value !== "Finish" ? (
+            <button
+              className="rounded-full border text-[#3A0CA3] bg-white hover:bg-[#3A0CA3] hover:text-white transition-all duration-400 group"
+              onClick={handleNextClick}
+              disabled={isCurrentSlideSelected() ? false : true}
+            >
+              <NextButton />
+              <span className="hidden opacity-0 absolute transform  bg-black text-white px-2.5 py-1 rounded whitespace-nowrap transition-opacity duration-200 group-hover:block group-hover:opacity-100">
+                NEXT
+              </span>
+            </button>
+          ) : (
+            <button onClick={handlePlayClick} className="group">
+              <img src="Play.svg" alt="Play" />
+              <span className="hidden opacity-0 absolute transform  bg-black text-white px-2.5 py-1 rounded whitespace-nowrap transition-opacity duration-200 group-hover:block group-hover:opacity-100">
+                PLAY
+              </span>
+            </button>
+          )}
+        </div>
       </div>
     </main>
   );
