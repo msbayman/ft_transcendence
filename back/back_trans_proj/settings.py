ee24690958f4dd4827f4fe950f547bd37b149a09
msaoub ayman <aymanmsaoub@gmail.com>
2025-01-21 14:51:37 +0100

    fix volumes backend

--- back/back_trans_proj/settings.py
+++ back/back_trans_proj/settings.py
@@ -220,8 +220,10 @@ CHANNEL_LAYERS = {
         },
     },
 }
-# MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_URL = '/media/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = '/app/media'  
+
 # CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_ALL_ORIGINS = True
