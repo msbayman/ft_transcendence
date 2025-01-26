dce56e5c44d4d7aa4da01d4a14c5fd4bde26dde5
Abdellah-ElKacem <abdellah.elkacem@gmail.com>
2025-01-26 02:00:06 +0100

    add tournaments page

--- front/src/pages/Tournaments/Tournaments.tsx
+++ front/src/pages/Tournaments/Tournaments.tsx
@@ -17,13 +17,54 @@ const Tournaments = () => {
         <div className="flex flex-col gap-[80px] w-[100%] h-[100%] justify-center items-center max-h-[1100px]">
           {/* -------------------------------------------------->>> part2 - 1 final */}
 
-          <div className="flex justify-center items-center rounded-[30px] bg-white w-[430px] h-[240px]">
-            final
+          <div className="relative flex flex-row justify-around items-center rounded-[30px] bg-white w-[400px] max-h-[180px]">
+            <div className="flex pl-3 flex-col gap-4 justify-around items-center">
+              <img
+                src={my_data.playerData?.profile_image.replace(
+                  "http://",
+                  "https://"
+                )}
+                alt="photo_profile"
+                className="w-[100px] rounded-[50%]"
+              />
+              <div className="font-alexandria text-[25px]">
+                {my_data.playerData?.username}
+              </div>
+            </div>
+            <div className="bg-[#3A0CA3] h-[80%] w-[5px] rounded-3xl"></div>
+            <div className="relative flex flex-col justify-around items-center pr-2">
+              <img
+                src="/Tournaments/win_tournaments.svg"
+                alt="win_tournament"
+                className=" w-[110px] pb-4"
+              />
+              <div className=" font-alexandria font-thin text-[25px] pb-7">
+                {" "}
+                {my_data.playerData?.points} Points
+              </div>
+            </div>
           </div>
           {/* -------------------------------------------------->>> part2 - 2 qualif_1 ------------------------------------------------------------------*/}
           <div className="flex flex-row justify-around items-start gap-[30px] w-full">
             <div className="flex flex-col items-center gap-[20px] justify-center h-[100%] w-2/6 ">
-              <div className="flex justify-center items-center h-[280px] w-[40%] bg-white rounded-[40px]"></div>
+              <div className="flex justify-center items-center h-[280px] w-[40%] bg-white rounded-[40px]">
+                <div className="flex flex-col gap-4 justify-around items-center">
+                  <img
+                    src={my_data.playerData?.profile_image.replace(
+                      "http://",
+                      "https://"
+                    )}
+                    alt="photo_profile"
+                    className="w-[120px] rounded-[50%] shadow-lg"
+                  />
+                  <div className="font-alexandria text-[25px]">
+                    {my_data.playerData?.username}
+                  </div>
+                  <div className="font-alexandria text-[25px] font-extralight">
+                    Level {my_data.playerData?.level}
+                  </div>
+                </div>
+              </div>
 
               {/* -------------------- start style hr ---------------------------- */}
               <div className="flex flex-col justify-center items-center h-[120px] w-[100%]">
@@ -37,8 +78,42 @@ const Tournaments = () => {
               {/* -------------------- end style hr ---------------------------- */}
 
               <div className="flex flex-row justify-between items-center h-[280px] w-[100%]">
-                <div className="h-[100%] w-[40%] bg-white rounded-[30px]"></div>
-                <div className="h-[100%] w-[40%] bg-white rounded-[30px]"></div>
+                <div className="flex justify-center items-center h-[100%] w-[40%] bg-white rounded-[30px]">
+                  <div className="flex flex-col gap-4 justify-around items-center">
+                    <img
+                      src={my_data.playerData?.profile_image.replace(
+                        "http://",
+                        "https://"
+                      )}
+                      alt="photo_profile"
+                      className="w-[120px] rounded-[50%] shadow-lg"
+                    />
+                    <div className="font-alexandria text-[25px]">
+                      {my_data.playerData?.username}
+                    </div>
+                    <div className="font-alexandria text-[25px] font-extralight">
+                      Level {my_data.playerData?.level}
+                    </div>
+                  </div>
+                </div>
+                <div className="flex justify-center items-center h-[100%] w-[40%] bg-white rounded-[30px]">
+                  <div className="flex flex-col gap-4 justify-around items-center">
+                    <img
+                      src={my_data.playerData?.profile_image.replace(
+                        "http://",
+                        "https://"
+                      )}
+                      alt="photo_profile"
+                      className="w-[120px] rounded-[50%] shadow-lg"
+                    />
+                    <div className="font-alexandria text-[25px]">
+                      {my_data.playerData?.username}
+                    </div>
+                    <div className="font-alexandria text-[25px] font-extralight">
+                      Level {my_data.playerData?.level}
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
 
@@ -49,7 +124,24 @@ const Tournaments = () => {
             </div>
             {/* -------------------------------------------------->>> part2 - 2 qualif_2 -----------------------------------------------------------*/}
             <div className="flex flex-col items-center gap-[20px] justify-center h-[100%] w-2/6 ">
-              <div className="flex justify-center items-center h-[280px] w-[40%] bg-white rounded-[40px]"></div>
+              <div className="flex justify-center items-center h-[280px] w-[40%] bg-white rounded-[40px]">
+                <div className="flex flex-col gap-4 justify-around items-center">
+                  <img
+                    src={my_data.playerData?.profile_image.replace(
+                      "http://",
+                      "https://"
+                    )}
+                    alt="photo_profile"
+                    className="w-[120px] rounded-[50%] shadow-lg"
+                  />
+                  <div className="font-alexandria text-[25px]">
+                    {my_data.playerData?.username}
+                  </div>
+                  <div className="font-alexandria text-[25px] font-extralight">
+                    Level {my_data.playerData?.level}
+                  </div>
+                </div>
+              </div>
 
               {/* -------------------- start style hr ---------------------------- */}
               <div className="flex flex-col justify-center items-center h-[120px] w-[100%]">
@@ -63,15 +155,49 @@ const Tournaments = () => {
               {/* -------------------- end style hr ---------------------------- */}
 
               <div className="flex flex-row justify-between items-center h-[280px] w-[100%]">
-                <div className="h-[100%] w-[40%] bg-white rounded-[30px]"></div>
-                <div className="h-[100%] w-[40%] bg-white rounded-[30px]"></div>
+                <div className="flex justify-center items-center h-[100%] w-[40%] bg-white rounded-[30px]">
+                  <div className="flex flex-col gap-4 justify-around items-center">
+                    <img
+                      src={my_data.playerData?.profile_image.replace(
+                        "http://",
+                        "https://"
+                      )}
+                      alt="photo_profile"
+                      className="w-[120px] rounded-[50%] shadow-lg"
+                    />
+                    <div className="font-alexandria text-[25px]">
+                      {my_data.playerData?.username}
+                    </div>
+                    <div className="font-alexandria text-[25px] font-extralight">
+                      Level {my_data.playerData?.level}
+                    </div>
+                  </div>
+                </div>
+                <div className="flex justify-center items-center h-[100%] w-[40%] bg-white rounded-[30px]">
+                  <div className="flex flex-col gap-4 justify-around items-center">
+                    <img
+                      src={my_data.playerData?.profile_image.replace(
+                        "http://",
+                        "https://"
+                      )}
+                      alt="photo_profile"
+                      className="w-[120px] rounded-[50%] shadow-lg"
+                    />
+                    <div className="font-alexandria text-[25px]">
+                      {my_data.playerData?.username}
+                    </div>
+                    <div className="font-alexandria text-[25px] font-extralight">
+                      Level {my_data.playerData?.level}
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
 
           {/* -------------------------------------------------->>> part3 button*/}
 
-          <div className="flex justify-center items-center bg-white text-[#3A0CA3] w-[220px] h-[70px] rounded-[30px] font-alexandria text-4xl">
+          <div className="flex justify-center items-center bg-white text-[#3A0CA3] w-[180px] h-[70px] rounded-[30px] font-alexandria text-2xl">
             Play
           </div>
         </div>
