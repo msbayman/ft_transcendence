32c9aeb026eaea9fd66fe3069548a51f13aa81a1
ilasrarf <aae.ilyass1890@gmail.com>
2025-01-28 03:03:12 +0100

    sdf

--- front/src/pages/Game_Page/Game_Torn.tsx
+++ front/src/pages/Game_Page/Game_Torn.tsx
@@ -1,14 +1,15 @@
 import Cookies from 'js-cookie';
 import { usePlayer } from '../My_Profile/PlayerContext';
 import Game_Remot from "./Game_Remot";
-import { useEffect, useState } from 'react';
+import { useEffect, useState, createContext, useContext } from 'react';
 import Player_Profil from '../../components/ui/game_comp/loby_profil';
+import { TournContext } from './TournContext';
 
 function Game_Tourn() {
-    
+    const matchdata = useContext(TournContext);
+
     return (
         <>
-            
         </>
     );
 }
--- front/src/pages/Game_Page/Test.tsx
+++ front/src/pages/Game_Page/Test.tsx
@@ -1,52 +1,28 @@
 import React, { useEffect, useState } from "react";
 import Cookies from "js-cookie";
+import Game_Tourn from "./Game_Torn";
+import { TournContext } from "./TournContext";
 
-
+	
 function Test(){
 
-  interface Match {
-    id: number;
-    player1: string;
-    player2: string;
-    player1_score: number;
-    player2_score: number;
-  }
+	const [tournamentState, setTournamentState] = useState({
+		p1: "player1",
+		p2: "player2",
+		p3: "player3",
+		p4: "player4",
+		semi1: "",
+		semi2: "",
+		final: "",
+	  });
 
-  const [matchs, setmatchs] = useState<Match[]>([])
-
-  const fetchData = async () => {
-    try {
-      const token = Cookies.get("access_token");
-      const resp = await fetch("http://127.0.0.1:8000/api/game/getdata/",
-      {
-		headers: {
-		  Authorization: `Bearer ${token}`,
-		}
-      }
-      )
-      const data = await resp.json()
-      setmatchs(data)
-    } catch (e) {
-      console.log(e)
-    }
-  };
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  return (
-    <div>
-      <div>test page</div>
-      {matchs.map((match) => <div>
-        <p> id: {match.id} </p> 
-        <p> p1: {match.player1} </p>
-        <p> p2: {match.player2} </p>
-        <p> sc1: {match.player1_score} </p>
-        <p> sc2: {match.player2_score} </p>
-      </div> )}
-    </div>
-    
-  );
+	return (
+		<>
+			<TournContext.Provider value={tournamentState}>
+      			<Game_Tourn />
+    		</TournContext.Provider>
+		</>
+	);
 };
 
 export default Test;
