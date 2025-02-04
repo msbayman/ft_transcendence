a28e4b8f5499446b1c36e9ce206db07b302d5a91
ayman <aymanmsaoub@gmail.com>
2025-02-04 13:12:22 +0100

    i cant serve dist

--- front/src/pages/Game_Page/TournContext.tsx
+++ front/src/pages/Game_Page/TournContext.tsx
@@ -11,12 +11,27 @@ interface TournInterface {
   finish: boolean;
 }
 
+interface SelectedId {
+  mode: number | null;
+  board: number | null;
+  paddel: number | null;
+  ball: number | null;
+  selectedStatus: {
+    mode: number[];
+    board: number[];
+    paddel: number[];
+    ball: number[];
+  };
+}
+
 interface TournContextType {
   tournamentState: TournInterface;
   setTournamentState: React.Dispatch<React.SetStateAction<TournInterface>>;
+  selectedId: SelectedId;
+  setSelectedId: React.Dispatch<React.SetStateAction<SelectedId>>;
 }
 
-const defaultValue: TournInterface = {
+const defaultTournamentState: TournInterface = {
   p1: "def-1",
   p2: "def-2",
   p3: "def-3",
@@ -27,17 +42,33 @@ const defaultValue: TournInterface = {
   finish: false,
 };
 
+const defaultSelectedId: SelectedId = {
+  mode: null,
+  board: null,
+  paddel: null,
+  ball: null,
+  selectedStatus: {
+    mode: [],
+    board: [],
+    paddel: [],
+    ball: [],
+  },
+};
+
 export const TournContext = createContext<TournContextType>({
-  tournamentState: defaultValue,
+  tournamentState: defaultTournamentState,
   setTournamentState: () => {},
+  selectedId: defaultSelectedId,
+  setSelectedId: () => {},
 });
 
 export const TournProvider = ({ children }: { children: ReactNode }) => {
-  const [tournamentState, setTournamentState] = useState<TournInterface>(defaultValue);
+  const [tournamentState, setTournamentState] = useState<TournInterface>(defaultTournamentState);
+  const [selectedId, setSelectedId] = useState<SelectedId>(defaultSelectedId);
 
   return (
-    <TournContext.Provider value={{ tournamentState, setTournamentState }}>
+    <TournContext.Provider value={{ tournamentState, setTournamentState, selectedId, setSelectedId }}>
       {children}
     </TournContext.Provider>
   );
-}
+};
