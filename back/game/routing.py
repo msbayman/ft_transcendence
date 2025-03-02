f93f549487352979e8b65c8391bc36a03869dfc2
alaex <otak3843@gmail.com>
2025-03-02 21:42:24 +0000

    add rsp consumer

--- back/game/routing.py
+++ back/game/routing.py
@@ -1,12 +1,15 @@
 from django.urls import re_path,path
 from .gameconsumers import GameConsumer
-# from .gameconsumers import SgMatchMakingConsumer 
+from .rsp import Rsp
+
+
 
 from .tournconsumer import TournConsumer
 from . import consumers
 
 websocket_urlpatterns = [
     re_path(r'ws/game/(?P<id>[\w_]+)/$', GameConsumer.as_asgi()),
+    re_path(r'ws/rsp/(?P<id>[\w_]+)/$', Rsp.as_asgi()),
     re_path(r'^ws/matchmaking/', consumers.MatchMakingConsumer.as_asgi()),
     re_path(r'^ws/matchmaking_sg/', consumers.SgMatchMakingConsumer.as_asgi()),
     re_path(r'^ws/challenge/(?P<id>[\w\-\+]+)/$', consumers.ChalleConsumer.as_asgi()),
